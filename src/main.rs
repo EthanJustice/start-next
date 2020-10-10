aad6c3bf0479dc3c951d30ebeb59f0b5ef8fa8aa
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-10-10 15:49:44 -0400

    chore: rocket setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// std
+
+// external
+use rocket::*; // glob for now
+use rocket_contrib::{serve::StaticFiles, templates::Template};
+
+// local
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount(
+            "/",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")),
+        )
+        .launch();
 }
